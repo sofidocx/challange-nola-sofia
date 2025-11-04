52844d49dba66d39a6aadd1028a5a1c1e0986a7a
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-11-03 21:26:27 -0300

    feat: adicionando variavel default para dev local

--- nola-frontend/src/main.tsx
+++ nola-frontend/src/main.tsx
@@ -8,7 +8,7 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'; 
 
-const CUBEJS_TOKEN = import.meta.env.VITE_CUBEJS_TOKEN;
+const CUBEJS_TOKEN = import.meta.env.VITE_CUBEJS_TOKEN || '6066b66a059206035b0330e54da3794fa63e53bb5af0b889c535f482ba0bbf09a41a0f12026a6a25662edae48d22d184f821a16dbfec84f57733ed59d18271e3';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/cubejs-api/v1';
 
 const cubeApi = cubejs(CUBEJS_TOKEN, {
